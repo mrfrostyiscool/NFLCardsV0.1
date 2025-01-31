e36c9c7354a673b835896578fca1f1e4ad1e5859
Frosty <zackmorlan@yahoo.com>
2025-01-30 18:32:29 -0700

    first commit

---
+++ media/lua/server/cardpack_recipes.lua
@@ -0,0 +1,49 @@
+local card_common={
+"NFLCards.AJBrown",
+"NFLCards.BijanRobinson",
+"NFLCards.DavanteAdams",
+"NFLCards.ChristainMcCaffrey",
+"NFLCards.DKMetcalf",
+"NFLCards.GeorgeKittle",
+}
+    
+local card_uncommon={
+"NFLCards.JoeBurrow",
+"NFLCards.TravisKelce",
+"NFLCards.TyreekHill",
+"NFLCards.LamarJackson",
+"NFLCards.PukaNacua",
+"NFLCards.TuckerKraft",
+}
+    
+local card_rare={
+"NFLCards.JustinJefferson",
+"NFLCards.MikeEvans",
+"NFLCards.JoshAllen",
+"NFLCards.DerrickHenry",
+"NFLCards.TomBrady",
+}
+    
+local card_epic={
+"NFLCards.RandyMoss",
+"NFLCards.JoeMontana",
+"NFLCards.AdrianPeterson",
+}
+    
+function openNFLPack(items, result, player)
+local roll=ZombRand(0,#card_common)
+player:getInventory():AddItem(card_common[roll+1])
+local roll=ZombRand(0,#card_uncommon)
+player:getInventory():AddItem(card_uncommon[roll+1])
+local roll=ZombRand(0,#card_rare)
+player:getInventory():AddItem(card_rare[roll+1])
+end
+    
+function openNFLPackGold(items, result, player)
+local roll=ZombRand(0,#card_rare)
+player:getInventory():AddItem(card_rare[roll+1])
+local roll=ZombRand(0,#card_epic)
+player:getInventory():AddItem(card_epic[roll+1])
+local roll=ZombRand(0,#card_epic)
+player:getInventory():AddItem(card_epic[roll+1])
+end
